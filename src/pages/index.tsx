2e0ccf5a4960f9494d263f127308ba6a2da97837
wanmao <wequart@gmail.com>
2021-10-06 23:14:23 +0800

    1. 添加gatsby配置文件 2. 使用useStaticQuery钩子和graphql查询配置文件的数据 3. 使用泛型

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,35 @@
 import * as React from 'react';
 import { graphql, Link, useStaticQuery } from 'gatsby';
 
-const App = () => {
-  const data = useStaticQuery(graphql`
-   query MyQuery {
-    site(siteMetadata: {}) {
-      siteMetadata {
-        description
-        title
-      }
+interface MyQuery {
+  site: {
+    siteMetadata: {
+      description: string
+      title: string
     }
-  }
-`);
+  };
+}
+
+const App = () => {
+  const data = useStaticQuery<MyQuery>(graphql`
+      query MyQuery {
+          site(siteMetadata: {}) {
+              siteMetadata {
+                  description
+                  title
+              }
+          }
+      }
+  `);
   
-  console.log('data: ', data)
+  console.log('data: ', data);
   const title = data?.site?.siteMetadata?.title ?? {}
   return (
     <>
       <header>
         <Link to={'/about'}>{title}</Link>
       </header>
-    <main>hello world! this is main page</main>
+      <main>hello world! this is main page</main>
     </>
   );
 };
