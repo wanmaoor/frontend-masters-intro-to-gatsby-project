6fb26f68b52b4943700ca667257d6d2cb3ae9862
wanmao <wequart@gmail.com>
2021-10-06 22:05:02 +0800

    create pages

---
+++ src/pages/About.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+const About = () => {
+  return (<main> this is about page</main>);
+}
+
+export default About
+
---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+const App = () => {
+  return (<main>hello world! this is main page</main>);
+};
+
+export default App;
+
